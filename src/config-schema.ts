0ead31f579bf1fb8dfa843dee53325fdc0d1d2b2
CynthiaKamau <cynthiakamau54@gmail.com>
2024-01-31 23:43:17 +0300

    O3-2778 Add config for different currency types

--- src/config-schema.ts
+++ src/config-schema.ts
@@ -1,3 +1,12 @@
-export interface BillingConfig {}
+import { Type } from '@openmrs/esm-framework';
 
-export const configSchema = {};
+export const configSchema = {
+  defaultCurrency: {
+    _type: Type.String,
+    _description: 'The default currency for the application. Specify the currency code (e.g., KES, UGX, GBP).',
+    _default: 'KES',
+  },
+};
+export interface ConfigObject {
+  defaultCurrency: string;
+}
