7ff74c6500869b9a41968ea85cc1ef9374546e3b
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2024-02-15 13:29:15 +0300

    PR comments

--- src/billable-services/dashboard/dashboard.component.tsx
+++ src/billable-services/dashboard/dashboard.component.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import BillableServices from '../billable-services.component';
-import ServiceMetrics from './service-metrics.component';
 import styles from './dashboard.scss';
 import { ExtensionSlot } from '@openmrs/esm-framework';
 
 export default function BillableServicesDashboard() {
   return (
     <main className={styles.container}>
-      <ServiceMetrics />
+      <ExtensionSlot name="billing-home-tiles-slot" />
       <main className={styles.servicesTableContainer}>
         <BillableServices />
       </main>
-      <ExtensionSlot name="billing-home-tiles-slot" />
     </main>
   );
 }
