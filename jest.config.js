e663eec205df5e650894ee268423de61bcab126b
Dennis Kigen <kigen.work@gmail.com>
2025-04-24 22:56:34 +0300

    (chore) Add automated dependency update workflow (#82)
    * (chore) Add automated dependency update workflow
    
    Adds a CI workflow and a corresponding bash script that automatically check for and update core OpenMRS dependencies. Other changes include:
    
    - Moving several existing configuration files such as the i18next parser config and the Jest test setup file into a top-level `tools` directory to align with code organization practices we follow across other O3 repos.
    
    * Update update-openmrs-deps.mjs
    
    ---------
    
    Co-authored-by: Ian <52504170+ibacher@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /**
  * @returns {Promise<import('jest').Config>}
  */
@@ -26,7 +27,7 @@ module.exports = {
     '^react-i18next$': path.resolve(__dirname, '__mocks__', 'react-i18next.js'),
     '^uuid$': path.resolve(__dirname, 'node_modules', 'uuid', 'dist', 'index.js'),
   },
-  setupFilesAfterEnv: ['<rootDir>/src/setup-tests.ts'],
+  setupFilesAfterEnv: [path.resolve(__dirname, 'tools', 'setup-tests.ts')],
   testPathIgnorePatterns: [path.resolve(__dirname, 'e2e')],
   testEnvironment: 'jsdom',
   testEnvironmentOptions: {
