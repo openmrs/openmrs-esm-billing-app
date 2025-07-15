a166f672b40de11e03eff4914a2222a27d23fd80
suubi-joshua <78343336+suubi-joshua@users.noreply.github.com>
2025-07-15 21:26:35 +0300

    Update sample-test.spec.ts

--- e2e/specs/sample-test.spec.ts
+++ e2e/specs/sample-test.spec.ts
@@ -7,5 +7,5 @@ import { expect } from '@playwright/test';
 test('Sample test', async ({ page }) => {
   const homePage = new HomePage(page);
   await homePage.goto();
-  await expect(homePage.page.getByRole('link', { name: 'service-queues' })).toBeVisible();
+  await expect(homePage.page.getByRole('link', { name: 'Service queues' })).toBeVisible();
 });
