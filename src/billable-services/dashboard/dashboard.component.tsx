2a7923a7733b5a40f5ee084a287da94af547d8f2
Dennis Kigen <kigen.work@gmail.com>
2025-10-17 21:04:44 +0300

    Remove empty extension slot

--- src/billable-services/dashboard/dashboard.component.tsx
+++ src/billable-services/dashboard/dashboard.component.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import BillableServices from '../billable-services.component';
 import styles from './dashboard.scss';
-import { ExtensionSlot } from '@openmrs/esm-framework';
 
 export default function BillableServicesDashboard() {
   return (
     <main className={styles.container}>
-      <ExtensionSlot name="billing-home-tiles-slot" />
       <main className={styles.servicesTableContainer}>
         <BillableServices />
       </main>
