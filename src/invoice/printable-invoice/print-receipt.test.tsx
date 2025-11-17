02477be72da46f01da07ea17478b2baed38bd9e8
mozzy11 <mozzymutesa@gmail.com>
2025-11-18 00:46:40 +0300

    fetch bill reciept by uuid

--- src/invoice/printable-invoice/print-receipt.test.tsx
+++ src/invoice/printable-invoice/print-receipt.test.tsx
@@ -9,14 +9,14 @@ describe('PrintReceipt', () => {
   });
 
   it('renders button with correct text and icon', () => {
-    render(<PrintReceipt billId={123} />);
+    render(<PrintReceipt billUuid={'a0655e54-126b-4b88-8c7c-579cb4f331f2'} />);
     expect(screen.getByText('Print receipt')).toBeInTheDocument();
   });
 
   it('displays "Loading" and disables button when isRedirecting is true', async () => {
     const user = userEvent.setup();
 
-    render(<PrintReceipt billId={123} />);
+    render(<PrintReceipt billUuid={'a0655e54-126b-4b88-8c7c-579cb4f331f2'} />);
 
     const button = screen.getByRole('button');
 
@@ -28,12 +28,12 @@ describe('PrintReceipt', () => {
   it('applies correct CSS class to button', async () => {
     const user = userEvent.setup();
 
-    render(<PrintReceipt billId={123} />);
+    render(<PrintReceipt billUuid={'a0655e54-126b-4b88-8c7c-579cb4f331f2'} />);
     expect(screen.getByRole('button')).toHaveClass('button');
   });
 
   it('translates button text correctly', () => {
-    render(<PrintReceipt billId={123} />);
+    render(<PrintReceipt billUuid={'a0655e54-126b-4b88-8c7c-579cb4f331f2'} />);
     expect(screen.getByText('Print receipt')).toBeInTheDocument();
   });
 });
