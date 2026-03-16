abd62348a7092ab54036d2af37f1ca7e8646cf67
vivektarun <vivektarun1234@gmail.com>
2026-03-17 02:40:00 +0530

    (fix) O3-5504: Remove super user button from billing header

--- src/billing-header/billing-header.component.tsx
+++ src/billing-header/billing-header.component.tsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import dayjs from 'dayjs';
 import { DatePickerInput, DatePicker } from '@carbon/react';
 import { useTranslation } from 'react-i18next';
-import { Location, UserFollow } from '@carbon/react/icons';
+import { Location } from '@carbon/react/icons';
 import { useSession } from '@openmrs/esm-framework';
 import { omrsDateFormat } from '../constants';
 import BillingIllustration from './billing-illustration.component';
@@ -29,10 +29,6 @@ const BillingHeader: React.FC<BillingHeaderProps> = ({ title }) => {
         </div>
       </div>
       <div className={styles['right-justified-items']}>
-        <div className={styles.userContainer}>
-          <p>{session?.user?.person?.display}</p>
-          <UserFollow size={16} className={styles.userIcon} />
-        </div>
         <div className={styles['date-and-location']}>
           <Location size={16} />
           <span className={styles.value}>{location}</span>
